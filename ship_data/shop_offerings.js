8b7dc97503fbd3290300e500fe7df350e479f994
eeoooue <pdmoorhouse@gmail.com>
2023-01-22 23:02:22 +0000

    added more ships, tiles now populate from list of ships

---
+++ ship_data/shop_offerings.js
@@ -0,0 +1,122 @@
+
+
+export function GetCoreDataShips() {
+
+    const ships = [
+        "Illustrious µ",
+        "Roon µ",
+        "Musketeer",
+        "Scharnhorst",
+        "West Virginia",
+        "Emile Bertin",
+        "Kongou",
+        "Haruna",
+        "Mutsu",
+        "Independence",
+        "Makinami",
+        "Astoria",
+        "Akatsuki",
+        "Fiji",
+        "Wasp",
+        "Le Mars",
+    ]
+
+    return ships
+}
+
+
+export function GetGuildShips() {
+
+    const ships = [
+        "Rare",
+        "Fletcher",
+        "Phoenix",
+        "Portland",
+        "Pennsylvania",
+        "Amazon",
+        "Shouhou",
+        "Jamaica",
+        "Elite",
+        "Helena",
+        "Cleveland",
+        "London",
+        "Arizona",
+        "Unicorn",
+        "Vestal",
+        "Dace",
+        "Colorado",
+        "Maryland",
+        "Admiral Graf Spee",
+        "Ning Hai",
+        "Ping Hai",
+        "Matchless",
+        "Sheffield",
+        "Yoizuki",
+        "Harutsuki",
+        "Admiral Hipper",
+        "U-557",
+        "Limited Event",
+        "Benson",
+        "Bailey",
+        "Yuugure",
+        "Hatsuharu",
+        "Jupiter",
+        "Jersey",
+        "Matsukaze",
+        "Wakaba",
+        "Urakaze",
+        "Isokaze",
+        "Michishio",
+        "Ariake",
+        "Hatsushimo",
+        "Z18",
+        "Bush",
+        "Arashio",
+        "Memphis",
+        "Hazelwood",
+        "Nagatsuk",
+    ]
+    
+    return ships
+}
+
+
+export function GetMeritShips() {
+
+    const ships = [
+        "Prototype Bulin MKII",
+        "Universal Bulin",
+        "South Dakota",
+        "Eldridge",
+        "Albacore",
+        "King George V",
+        "Duke of York",
+        "Littorio",
+        "Le Malin µ",
+        "Grenville",
+        "Sheffield µ",
+        "Z26",
+        "Z36",
+        "Quincy",
+        "Vincennes",
+        "Le Temeraire",
+        "Carabiniere",
+        "Bataan",
+        "Ryuujou",
+        "Kinu",
+        "Hibiki",
+        "Z28",
+        "Kuroshio",
+        "Oyashio",
+        "Echo",
+        "Conte di Cavour",
+        "Trento",
+        "San Juan",
+        "Newcastle",
+        "Bluegill",
+        "Casablanca",
+        "Hunter",
+    ]
+
+    return ships
+}
