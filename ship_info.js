dda4a14d937090bf4646714e2c01b87194f1a584
eeoooue <pdmoorhouse@gmail.com>
2023-01-22 16:30:02 +0000

    added map drop ships to ship info

--- ship_info.js
+++ ship_info.js
@@ -16,51 +16,205 @@ export class ShipCodex {
 
 
         this.learnMapDrops()
-
-
     }
 
 
     learnMapDrops(){
 
-        const Chapter01 = ["Comet", "Craven", "Crescent", "Cygnet", "McCall"]
-        const Chapter02 = []
-        const Chapter03 = []
-        const Chapter04 = []
-        const Chapter05 = []
-
-        const Chapter06 = []
-        const Chapter07 = []
-        const Chapter08 = []
-        const Chapter09 = []
-        const Chapter10 = []
-
-        const Chapter11 = []
-        const Chapter12 = []
-        const Chapter13 = []
-        const Chapter14 = []
-
-
+        const Chapter_1 = [
+            "Comet",
+            "Craven",
+            "Crescent",
+            "Cygnet",
+            "McCall",
+            "Aulick",
+            "Foote",
+            "Foxhound",
+            "Spence",
+            "Cassin",
+            "Downes",
+            "Renown",
+            "Long Island",
+            "Repulse",
+            "Leander",
+            "Omaha",
+            "Raleigh",
+        ]
+        
+        const Chapter_2 = [
+            "Ayanami",
+            "Javelin",
+            "Laffey",
+            "London",
+            "Z23",
+            "California",
+            "Juno",
+            "Tennessee",
+            "Bogue",
+            "Hermes",
+            "Pensacola",
+            "Arizona",
+            "Brooklyn",
+            "Gridley",
+            "Pennsylvania",
+            "Shouhou",
+            "Nevada",
+            "Oklahoma",
+            "Nelson",
+            "Fletcher",
+            "Norfolk",
+            "Thatcher",
+            "Lexington",
+            "Rodney",
+            "Achilles",
+            "Ajax",
+            "Kent",
+            "Suffolk",
+            "Beagle",
+            "Bulldog",
+        ]
+        
+        const Chapter_3 = [
+            "Hiryuu",
+            "Houston",
+            "Souryuu",
+            "Arethusa",
+            "Chicago",
+            "Galatea",
+            "Northampton",
+            "Langley",
+            "Ranger",
+            "Charles Ausburne",
+            "Fortune",
+            "Hammann",
+            "Sims",
+            "Karlsruhe",
+            "Koln",
+            "Konigsberg",
+            "Indianapolis",
+            "Kagerou",
+            "Shiratsuyu",
+            "Furutaka",
+            "Kako",
+            "Akagi",
+            "Kaga",
+            "Yorktown",
+            "Portland",
+        ]
+        
+        
+        const Chapter_4 = [
+            "Saratoga",
+            "Phoenix",
+            "Shiranui",
+            "Shigure",
+            "Shropshire",
+            "Erebus",
+            "Terror",
+            "Cleveland",
+            "Unicorn",
+            "Amazon",
+        ]
+        
+        const Chapter_5 = [
+            "Helena",
+            "Z1",
+            "Myoukou",
+            "Glowworm",
+            "Queen Elizabeth",
+            "Atlanta",
+            "Juneau",
+            "Hornet",
+        ]
+        
+        const Chapter_6 = [
+            "Vestal",
+            "Ikazuchi",
+            "Inazuma",
+            "Exeter",
+            "York",
+            "Fusou",
+            "Yamashiro",
+            "Ark Royal",
+            "Yuudachi",
+        ]
+        
+        
+        const Chapter_7 = [
+            "Isuzu",
+            "Houshou",
+        ]
+        
+        const Chapter_8 = [
+            "Abukuma",
+            "Nachi",
+            "Salt Lake City",
+            "Maya",
+            "Richmond",
+        ]
+        
+        const Chapter_9 = [
+            "Jenkins",
+            "Radford",
+            "Nicholas",
+            "Niizuki",
+        ]
+        
+        const Chapter_10 = [
+            "Mikazuki",
+            "Minazuki",
+            "Honolulu",
+            "St. Louis",
+            "Jintsuu",
+        ]
+        
+        const Chapter_11 = [
+            "Agano",
+            "Sendai",
+            "Columbia",
+        ]
+        
+        const Chapter_12 = [
+            "Hiyou",
+            "Junyou",
+            "Choukai",
+        ]
+        
+        const Chapter_13 = [
+            "Dewey",
+            "Bunker Hill",
+        ]
+        
+        const Chapter_14 = [
+            "Ashigara",
+            "Denver",
+            "West Virginia",
+            "Michishio",
+            "New Orleans",
+            "Mogami",
+        ]
+        
         const AllChapters = [
-            Chapter01, 
-            Chapter02, 
-            Chapter03,
-            Chapter04,
-            Chapter05,
-            Chapter06,
-            Chapter07,
-            Chapter08,
-            Chapter09,
-            Chapter10,
-            Chapter11,
-            Chapter12,
-            Chapter13,
-            Chapter14,
+            Chapter_1, 
+            Chapter_2,
+            Chapter_3, 
+            Chapter_4,
+            Chapter_5, 
+            Chapter_6,
+            Chapter_7, 
+            Chapter_8,
+            Chapter_9, 
+            Chapter_10,
+            Chapter_11, 
+            Chapter_12,
+            Chapter_13,
+            Chapter_14,
         ]
 
         for(const Chapter of AllChapters){
             for(const shipname of Chapter){
                 this.MapDrops.add(shipname)
+                this.AllShips.add(shipname)
             }
         }
 
