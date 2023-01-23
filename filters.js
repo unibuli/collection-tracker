eb5b053a1f030bc98275d897a06e717faff4c6d1
eeoooue <pdmoorhouse@gmail.com>
2023-01-23 14:41:58 +0000

    added rightclick & resetall features for filters

--- filters.js
+++ filters.js
@@ -6,7 +6,66 @@ export class FilterManager {
     constructor() {
 
         this.codex = new ShipCodex()
+        new CollectionToggler()
+
+        this.activeFilters = new Set()
         this.initializeFilters()
+        this.addResetButton()
+    }
+
+    addResetButton() {
+
+        const ResetBtn = document.querySelector('.reset-filters')
+        ResetBtn.addEventListener("click", () => {
+            this.setAllFiltersTo("on")
+            this.filterDisplayedShips()
+        })
+    }
+
+    toggleCriteria(criteria) {
+
+        if (this.activeFilters.has(criteria)) {
+            this.activeFilters.delete(criteria)
+        } else {
+            this.activeFilters.add(criteria)
+        }
+    }
+
+    setAllFiltersTo(choice) {
+
+        const filters = document.querySelectorAll('.filter-tile')
+
+        for (const filter of filters) {
+            this.setFilterTo(filter, choice)
+        }
+    }
+
+    setFilterTo(filter, choice) {
+
+        switch (choice) {
+            case "on":
+                this.activateFilter(filter)
+                return
+            case "off":
+                this.deactivateFilter(filter)
+                return
+        }
+    }
+
+    activateFilter(filter) {
+
+        if (!filter.classList.contains('filter-active')) {
+            filter.classList.toggle('filter-active')
+            this.toggleCriteria(filter.innerText)
+        }
+    }
+
+    deactivateFilter(filter) {
+
+        if (filter.classList.contains('filter-active')) {
+            filter.classList.toggle('filter-active')
+            this.toggleCriteria(filter.innerText)
+        }
     }
 
     initializeFilters() {
@@ -16,9 +75,21 @@ export class FilterManager {
         for (const filter of filters) {
 
             filter.classList.add('filter-active')
+            this.toggleCriteria(filter.innerText)
 
+            // left click
             filter.addEventListener("click", () => {
                 filter.classList.toggle('filter-active')
+                this.toggleCriteria(filter.innerText)
+                this.filterDisplayedShips()
+            })
+
+            // right click
+            filter.addEventListener("contextmenu", (e) => {
+                e.preventDefault()
+                this.setAllFiltersTo("off")
+                filter.classList.toggle('filter-active')
+                this.toggleCriteria(filter.innerText)
                 this.filterDisplayedShips()
             })
         }
@@ -26,28 +97,39 @@ export class FilterManager {
 
     filterDisplayedShips() {
 
-        const filters = document.querySelectorAll('.filter-tile')
-        const activeFilters = new Set()
-
-        for (const filter of filters) {
-            if (filter.classList.contains('filter-active')) {
-                activeFilters.add(filter.innerText)
-            }
-        }
-
         const allships = document.querySelectorAll('.ship-tile')
-
         for (const ship of allships) {
-            ship.classList.add('hide-filter')
-            const shipname = ship.innerText
+            this.filterTile(ship)
+        }
+    }
 
-            for (const criteria of activeFilters) {
-                if (this.codex.shipIsAvailable(shipname, criteria)) {
-                    ship.classList.remove('hide-filter')
-                    break
-                }
+    filterTile(ship) {
+
+        const shipname = ship.innerText
+        ship.classList.add('hide-filter')
+
+        for (const criteria of this.activeFilters) {
+            if (this.codex.shipIsAvailable(shipname, criteria)) {
+                ship.classList.remove('hide-filter')
+                return
             }
         }
     }
 }
 
+class CollectionToggler {
+
+    constructor() {
+
+        const ShowCollectedBtn = document.querySelector('.collected-toggle')
+        ShowCollectedBtn.addEventListener("click", () => this.toggleCollectionDisplay())
+    }
+
+    toggleCollectionDisplay() {
+
+        const allships = document.querySelectorAll('.ship-tile')
+        for (const tile of allships) {
+            tile.classList.toggle('hide-collected')
+        }
+    }
+}
